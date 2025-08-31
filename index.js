b2bf0c5d84399c2053334eccb8afbf94f2fbf446
D-Eva-Code <anagwonyeadaeze42@gmail.com>
2025-08-31 16:29:31 +0100

    created function to assign short url count for new urls

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const { urlencoded } = require('body-parser');
 const app = express();
 
 // Basic Configuration
@@ -19,6 +20,51 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.use(express.urlencoded({extended:false}));
+
+
+app.get('/api/shorturl/:id',(req, res)=>{
+   const posturl= req.body.url;
+   let url= req.params.id;
+   
+  
+  if (posturl!==url){
+    res.json({error: "No short URL found for the given input"});  }
+    else{
+    res.redirect(posturl);
+  }
+
+})
+app.post('/api/shorturl/',(req, res)=>{
+  const posturl= req.body.url;
+  let counter=1;
+  let short_url={};
+
+function addcount(){
+  if (short_url[posturl]!==undefined){//if posturl already has a short url
+    return short_url[posturl]=counter;
+  }
+  else{
+      short_url[posturl]=counter++;//if its a new url assign it the current counter value
+      // counter++;
+      return short_url[posturl];
+  }
+
+  }
+  if (!posturl){
+    res.json({error: "invalid url"});
+  }
+
+  else{
+    res.json({"original_url":posturl, "short_url":addcount()});
+  }
+  req.params.id==counter;
+
+});
+ 
+  // let url= req.params.id;
+  // let short_url=1;
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
